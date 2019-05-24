dcdfc0a335255a4a42facefa07faeb2d7a9f502c
Dane Wicki <wicki.dane@gmail.com>
2019-05-24 06:59:09 +0200

    Add trailing new line

--- src/main/kotlin/ch/bda/baumannwicki/util/FilenameValidator.kt
+++ src/main/kotlin/ch/bda/baumannwicki/util/FilenameValidator.kt
@@ -4,7 +4,7 @@ import java.io.File
 import java.io.IOException
 
 class FilenameValidator {
-    fun isFilenameValid(filename: String):Boolean {
+    fun isFilenameValid(filename: String): Boolean {
         val f = File(filename)
         return try {
             f.canonicalPath
