3d5a33c85483b2ff5ab936a65bf4ac1818a64dfb
KilnOfTheSecondFlame <pbauma@gmx.net>
2019-05-26 21:20:24 +0200

    Implement requested changes

--- src/main/kotlin/ch/bda/baumannwicki/log/LogPersistorImplementation.kt
+++ src/main/kotlin/ch/bda/baumannwicki/log/LogPersistorImplementation.kt
@@ -5,25 +5,20 @@ import java.time.LocalDateTime
 import java.util.logging.FileHandler
 import java.util.logging.Logger
 
-class LogPersistorImplementation : LogPersistor {
-    private val dateTime = LocalDateTime.now()
-    private val fileHandler: FileHandler = FileHandler("./RFIDRefImplTestLog-${dateTime.year}.${dateTime.monthValue}.${dateTime.dayOfMonth}-${dateTime.hour}.${dateTime.minute}.${dateTime.second}.log")
-    private val logger = Logger.getLogger("RFIDRefImplLogger")
-
+class LogPersistorImplementation(fileHandler: FileHandler, val logger: Logger) : LogPersistor {
     init {
+        val dateTime = LocalDateTime.now()
         logger.addHandler(fileHandler)
         logger.info("Logger startet at ${dateTime.hour}.${dateTime.minute}.${dateTime.second}")
     }
 
     override fun noMisplacedTagFound(correctPlacedTags: List<LibraryCopy>) {
-        logger.warning("No Misplaced Tag Found")
+        logger.info("No Misplaced Tag Found")
         correctPlacedTags.stream().forEach { libraryCopy -> logger.info("Copy found: ${libraryCopy.article}") }
     }
 
     override fun misplacedTagFound(misplacedTag: List<LibraryCopy>, correctPlacedTags: List<LibraryCopy>) {
-        logger.warning("Misplaced Tag Found")
-        misplacedTag.stream().forEach { libraryCopy -> logger.warning("Misplaced Tag found: ${libraryCopy.article}") }
-        correctPlacedTags.stream().forEach { libraryCopy -> logger.info("Copy found: ${libraryCopy.article}") }
+        logger.info("MisplacedTag found: " + misplacedTag.map { libraryCopy -> "ArticleID: ${libraryCopy.article}, BoxID: ${libraryCopy.box}" })
+        logger.info("LibraryCopies found: " + correctPlacedTags.map { libraryCopy -> "ArticleID: ${libraryCopy.article}, BoxID: ${libraryCopy.box}" })
     }
-
 }
--- src/test/kotlin/ch/bda/baumannwicki/log/IntLogPersistorImplementationTest.kt
+++ src/test/kotlin/ch/bda/baumannwicki/log/IntLogPersistorImplementationTest.kt
@@ -2,19 +2,22 @@ package ch.bda.baumannwicki.log
 
 import ch.bda.baumannwicki.data.LibraryCopy
 import org.junit.jupiter.api.Test
+import java.time.LocalDateTime
+import java.util.logging.FileHandler
+import java.util.logging.Logger
 
 
 internal class IntLogPersistorImplementationTest {
+    val dateTime = LocalDateTime.now()
+    private var logPersistor : LogPersistorImplementation = LogPersistorImplementation(FileHandler("./RFIDRefImplIntegrationTestLog-${dateTime.year}.${dateTime.monthValue}.${dateTime.dayOfMonth}-${dateTime.hour}.${dateTime.minute}.${dateTime.second}.log"), Logger.getLogger("IntegrationTest"))
 
     @Test
     fun noMisplacedTagFound() {
-        val logPersistor = LogPersistorImplementation()
         logPersistor.noMisplacedTagFound(listOf(LibraryCopy("ABCD1","1","Book","Horizontal"),LibraryCopy("ABCD2","1","Book","Horizontal"),LibraryCopy("ABCD3","1","Book","Horizontal")))
     }
 
     @Test
     fun misplacedTagFound() {
-        val logPersistor = LogPersistorImplementation()
         logPersistor.misplacedTagFound(listOf(LibraryCopy("ZYXW1","1337","Book","Vertical")),listOf(LibraryCopy("ABCD1","1","Book","Horizontal"),LibraryCopy("ABCD2","1","Book","Horizontal"),LibraryCopy("ABCD3","1","Book","Horizontal")))
     }
 }
