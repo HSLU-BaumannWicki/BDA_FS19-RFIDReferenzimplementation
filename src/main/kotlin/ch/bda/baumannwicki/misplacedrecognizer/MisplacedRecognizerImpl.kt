0fe4429f964bba939b0f49d56a254aadebd073ac
Dane Wicki <wicki.dane@gmail.com>
2019-05-26 15:02:14 +0200

    Add implementation of getMisplacedTags

--- src/main/kotlin/ch/bda/baumannwicki/misplacedrecognizer/MisplacedRecognizerImpl.kt
+++ src/main/kotlin/ch/bda/baumannwicki/misplacedrecognizer/MisplacedRecognizerImpl.kt
@@ -1,4 +1,20 @@
 package ch.bda.baumannwicki.misplacedrecognizer
 
-class MisplacedRecognizerImpl {
+import ch.bda.baumannwicki.data.LibraryCopy
+
+class MisplacedRecognizerImpl : MisplacedRecognizer {
+    override fun getMostLikeleyBoxId(allTags: List<LibraryCopy>): List<LibraryCopy> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getMisplacedTags(allTags: List<LibraryCopy>): List<LibraryCopy> {
+        var map: MutableMap<String, Int> = HashMap<String, Int>()
+        for (tag in allTags) {
+            map[tag.box] = (map[tag.box] ?: 0) + 1
+        }
+        val resultList = map.toList().sortedBy { (_, value) -> value }.reversed().toMutableList()
+        resultList.removeAt(0)
+        val resultMap = resultList.toMap()
+        return allTags.filter { resultMap.containsKey(it.box) }.toList()
+    }
 }
--- src/test/kotlin/ch/bda/baumannwicki/misplacedrecognizer/MisplacedRecognizerImplTest.kt
+++ src/test/kotlin/ch/bda/baumannwicki/misplacedrecognizer/MisplacedRecognizerImplTest.kt
@@ -1,5 +1,79 @@
 package ch.bda.baumannwicki.misplacedrecognizer
 
+import ch.bda.baumannwicki.data.LibraryCopy
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
 class MisplacedRecognizerImplTest {
 
+    @Test
+    fun givenListWithOneTag_whenCalledGetMisplacedTags_thenReturnShouldBeEmptyList() {
+        val testee = MisplacedRecognizerImpl()
+        val result: List<LibraryCopy> = testee.getMisplacedTags(listOf(LibraryCopy("F", "1", "1", "2")))
+        assertEquals(emptyList(), result)
+    }
+
+    @Test
+    fun givenListTwoTagsWithSameContent_whenCalledGetMisplacedTags_thenReturnShouldBeEmptyList() {
+        val testee = MisplacedRecognizerImpl()
+        val result: List<LibraryCopy> = testee.getMisplacedTags(
+            listOf(
+                LibraryCopy("F", "1", "1", "2"),
+                LibraryCopy("F", "1", "1", "2")
+            )
+        )
+        assertEquals(emptyList(), result)
+    }
+
+    @Test
+    fun givenListThreeTagsWithOneTagWithADiferentBox_whenCalledGetMisplacedTags_thenReturnTheOneTagWithDifferentBox() {
+        val testee = MisplacedRecognizerImpl()
+        val result: List<LibraryCopy> = testee.getMisplacedTags(
+            listOf(
+                LibraryCopy("F", "1", "1", "2"),
+                LibraryCopy("F", "2", "1", "2"),
+                LibraryCopy("F", "2", "1", "2")
+            )
+        )
+        assertEquals(listOf(LibraryCopy("F", "1", "1", "2")), result)
+    }
+
+    @Test
+    fun givenTagListContaintingTwoWrongBoxes_whenCalledGetMisplacedTags_thenReturnTheTagsWithDifferentBox() {
+        val testee = MisplacedRecognizerImpl()
+        val result: List<LibraryCopy> = testee.getMisplacedTags(
+            listOf(
+                LibraryCopy("F", "2", "1", "2"),
+                LibraryCopy("F", "2", "1", "2"),
+                LibraryCopy("F", "5", "1", "2"),
+                LibraryCopy("F", "2", "1", "2"),
+                LibraryCopy("F", "6", "1", "2")
+            )
+        )
+        assertEquals(listOf(LibraryCopy("F", "5", "1", "2"), LibraryCopy("F", "6", "1", "2")), result)
+    }
+
+
+    class TagBuilder() {
+        fun buildTag(box: String): LibraryCopy {
+            return LibraryCopy("F", box, "1", "2")
+        }
+
+        fun buildListTags(box: String, number: Int): List<LibraryCopy> {
+            val list = ArrayList<LibraryCopy>()
+            for (i in 0..number) list.add(buildTag(box))
+            return list
+        }
+
+        fun buildListWithDifferentBoxes(
+            box1: String,
+            numberOfBoxOne: Int,
+            box2: String,
+            numberOfBoxTwo: Int
+        ): List<LibraryCopy> {
+            val list = buildListTags(box1, numberOfBoxOne).toMutableList()
+            list.addAll(buildListTags(box2, numberOfBoxTwo))
+            return list
+        }
+    }
 }
